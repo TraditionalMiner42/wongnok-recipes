7274ccf6124323e0e3cdb4f042bc46bb5bea272c
chayapon <chayapon.c@gmail.com>
2024-04-05 18:11:08 +0700

    First commit for recipe app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { goto } from "$app/navigation";
+
+	const directToMemberPage = () => goto("/member");
+	const directToGuestPage = () => goto("/guest");
+</script>
+
+<div class="app-header px-4 py-2 m-4">Wongnok Recipes</div>
+<div class="form-container">
+	<form class="sign-in-container" method="post">
+		<input class="email-input" placeholder="Enter your email" />
+		<input class="pw-input" placeholder="Enter your password" />
+		<button
+			class="sign-in-btn mx-[25em]"
+			on:click|preventDefault={directToMemberPage}>Sign In</button
+		>
+		<button
+			class="sign-in-btn mx-[25em]"
+			on:click|preventDefault={directToGuestPage}>View as Guest</button
+		>
+	</form>
+</div>
---
+++ src/routes/guest/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { goto } from "$app/navigation";
+
+	const toggleSignIn = () => goto("/");
+	const toggleSignUp = () => goto("/sign-up");
+</script>
+
+<div class="sign-in-up-container">
+	<button class="sign-in-btn" on:click={toggleSignIn}>Sign-in</button>
+	<button class="sign-up-btn" on:click={toggleSignUp}>Sign-up</button>
+</div>
+<div class="app-header">Wongnok Recipes</div>
